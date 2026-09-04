8d14b31e0d0a6bc23da2cafb5e6b7b9cd9a64b56
Bhavana <bhavanajoseph84392@gmail.com>
2026-09-05 00:35:51 +0530

    Connect frontend to Render backend

--- AutoVerse/src/services/api.js
+++ AutoVerse/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://autoverse-realworld-dark-1-2.onrender.com",
 });
 
 export default api;
